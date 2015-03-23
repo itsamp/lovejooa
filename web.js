767cf28d19b84e3c92fa9f89a02dda588e4a5042
Lewis <a20s@nate.com>
2015-03-24 01:06:31 +0900

    init heroku

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger()); 
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Heroku Node.js World!');
 }); 
 
 var port = process.env.PORT || 5000;
